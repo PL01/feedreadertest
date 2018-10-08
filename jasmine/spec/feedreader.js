7e33261e136a0311b51b5e22f6f54872c682cd03
Pablo Lara <pablolara01@yahoo.com>
2018-10-08 18:20:29 -0400

    Added Tests for loadFeed function and for new feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -51,7 +51,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-describe('The menu', function (){
+    describe('The menu', function (){
     
     /* TODO: Write a test that ensures the menu element is
     * hidden by default. You'll have to analyze the HTML and
@@ -78,21 +78,52 @@ describe('The menu', function (){
         menu.click();
         expect(body.classList.contains('menu-hidden')).toBe(true);
     });
-});
+    });
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function (done) {
+            loadFeed(0, done);
+        });
+
+        it('completes work', function() {
+            const feed = document.querySelector('.feed');
+            expect(feed.children.length > 0).toBe(true);
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-}());
+        const feed = document.querySelector('.feed');
+        const firstFeed = [];
+        
+        beforeEach(function(done) {
+            loadFeed(0);
+            //console.log(feed.children[0].innerText);
+            Array.from(feed.children).forEach(function(entry){
+                firstFeed.push(entry.innerText);
+            });
+            loadFeed(1, done);
+        });
+
+
+        it('content changes', function() {
+            //console.log(feed.children[0].innerText);
+            Array.from(feed.children).forEach(function(entry,index){
+                console.log(entry.innerText, firstFeed[index], entry.innerText === firstFeed[index]);
+                expect(entry.innerText === firstFeed[index]).toBe(false);
+            });
+        });
+    });
+});
